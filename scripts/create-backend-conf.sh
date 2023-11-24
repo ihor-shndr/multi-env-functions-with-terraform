a3b342325a5136715ec6cd1486ba3031029800b9
ihorshnaider <ihor_shnaider@outlook.com>
2023-11-24 11:53:24 +0200

    echo conf

--- scripts/create-backend-conf.sh
+++ scripts/create-backend-conf.sh
@@ -2,10 +2,16 @@ APP_NAME=$1
 ENV=$2
 STORAGE_ACCOUNT=$3
 
+echo "APP_NAME: $APP_NAME"
+echo "ENV: $ENV"
+echo "STORAGE_ACCOUNT: $STORAGE_ACCOUNT"
+
 RESOURCE_GROUP=tfstate-rg-$APP_NAME
 CONTAINER_KEY="${APP_NAME}.${ENV}"
 
 target_directory="../terraform/backend.conf"
+
+
 
 echo "storage_account_name = \"$STORAGE_ACCOUNT\"" > "$target_directory"
 echo "resource_group_name  = \"$RESOURCE_GROUP\"" >> "$target_directory"
